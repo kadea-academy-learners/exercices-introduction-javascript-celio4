cb011223f8ff8152a5c2f74a0894642c71d6b319
celio fofo <celiofofo30@gmail.com>
2025-09-24 20:00:32 +0100

    Exercice 7 : déclaration des variables et export pour tests

--- 03-operator-1/exercice_7/main.js
+++ 03-operator-1/exercice_7/main.js
@@ -1,2 +1,10 @@
-// Exercice 7
-// Écrivez votre code ici
+// Exercice 7 : calcul des âges avec opérateurs
+
+let ageEnfant = 10;                  // âge de l'enfant
+let agePere = ageEnfant * 2;         // âge du père = double de l'enfant
+let ageMere = agePere - 5;           // âge de la mère = père - 5
+let ageGrandPere = ageMere * 2 + ageEnfant / 2; // grand-père = double de la mère + moitié de l'enfant
+let ageOncle = agePere + 10;         // oncle = père + 10
+
+// Exporter les variables pour les tests
+module.exports = { ageEnfant, agePere, ageMere, ageGrandPere, ageOncle };
