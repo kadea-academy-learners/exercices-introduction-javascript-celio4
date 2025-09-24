706479304ec2f996bda4787ef04bc9e54b496f61
celio fofo <celiofofo30@gmail.com>
2025-09-24 15:58:30 +0100

    feat: exercice 2 terminé

--- 01-variables/exercice_2/main.js
+++ 01-variables/exercice_2/main.js
@@ -1,1 +1,13 @@
-//écrire votre code ici
+// Exercice 2 : Déclaration de variables
+
+// une chaîne de caractères
+let nom = "Fofo";
+
+// un nombre
+const myAge = 25;
+
+// un booléen
+const isStudent = true;
+
+module.exports = { myName, myAge, isStudent };
+
