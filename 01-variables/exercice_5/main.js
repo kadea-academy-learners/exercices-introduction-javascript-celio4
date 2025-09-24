2aafbf679c1cdfe6e181f2d9fb77cffaeaeb23f8
celio fofo <celiofofo30@gmail.com>
2025-09-24 19:24:37 +0100

    Exercice 5 : conversion Celsius -> Fahrenheit

--- 01-variables/exercice_5/main.js
+++ 01-variables/exercice_5/main.js
@@ -1,2 +1,6 @@
-// Exercice 5
-// Écrivez votre code ici
+// Exercice 5 : conversion Celsius -> Fahrenheit
+
+let celsius = 25; // température en Celsius
+let fahrenheit = (celsius * 9/5) + 32;
+
+console.log(fahrenheit);
